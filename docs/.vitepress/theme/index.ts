c503619e5c0f8d81bc213c2cef1476fe3d1b9f3d
Yousef Matar <yousefmatar@outlook.com>
2024-02-04 23:45:44 +0200

    added to top indicator for documentation

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,6 @@
 import MataritoVue from 'matarito-vue'
+import vitepressBackToTop from 'vitepress-plugin-back-to-top'
+import 'vitepress-plugin-back-to-top/dist/style.css'
 import DefaultTheme from 'vitepress/theme'
 import DemoContainer from '../components/DemoContainer.vue'
 import './demo-container.css'
@@ -8,5 +10,8 @@ export default {
    enhanceApp({ app }) {
      app.use(MataritoVue)
      app.component('DemoContainer', DemoContainer)
+     vitepressBackToTop({
+      threshold:300
+    })
    }
  }
