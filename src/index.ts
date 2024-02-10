16153047066ed9681884872a24b4cba5b2588ca0
Yousef Matar <yousefmatar@outlook.com>
2024-02-10 09:07:55 +0200

    config test

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { App } from "vue";
 import * as components from "./components";
 import * as directives from "./directives";
+// Vue.config.globalProperties.$appState =
 function install(app: App, options: { ripple: boolean }) {
+	app.config.globalProperties.$appState = { testingState: false };
 	for (const key in components) {
 		// @ts-expect-error error
 		app.component(key, components[key]);
 	}
 	for (const key in directives) {
 		if (!options.ripple && key == "Ripple") {
+			app.config.globalProperties.$appState["ripple"] = options.ripple;
 			continue;
 		}
 		// @ts-expect-error error
@@ -18,3 +21,4 @@ function install(app: App, options: { ripple: boolean }) {
 export default { install };
 export * from "./components";
 export * from "./directives";
+
