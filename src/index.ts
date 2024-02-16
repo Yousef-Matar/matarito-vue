0e6c4d48c29854ea3ceb64f1a10ce55ff96cd1bb
Yousef Matar <yousefmatar@outlook.com>
2024-02-16 10:37:27 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as components from "./components";
 import type { iConfig } from "./config";
 import { defaultConfig } from "./config";
 import * as directives from "./directives";
-function install(app: App, options: iConfig) {
+const install = (app: App, options: iConfig) => {
 	const applicationConfig = { ...defaultConfig, ...options };
 	app.config.globalProperties.$matarito = {};
 	app.config.globalProperties.$matarito.config = applicationConfig;
@@ -18,7 +18,7 @@ function install(app: App, options: iConfig) {
 			app.directive(key, directives[key]);
 		}
 	}
-}
+};
 
 export default { install };
 export * from "./components";
